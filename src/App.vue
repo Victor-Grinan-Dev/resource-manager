2a73d0b17745e2aaa423f117e5b794c5dbd8fa7d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-09 09:46:14 +0300

    ready product

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <TheHeader appTitle="Vue App" />
+    <the-resource></the-resource>
+  </div>
+</template>
+
+<script>
+import TheResource from "./components/learning-resources/TheResource.vue";
+import TheHeader from "./components/layout/TheHeader.vue";
+export default {
+  components: {
+    // StoredResources,
+    TheHeader,
+    TheResource,
+  },
+  methods: {},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
